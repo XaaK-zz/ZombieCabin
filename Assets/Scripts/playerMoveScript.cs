ebf0c35fd3942606e55bebb69139b455e411e3e0
XaaK <z_greenvoss@hotmail.com>
2020-04-03 22:16:39 -0700

    basic player movement added

---
+++ Assets/Scripts/playerMoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoveScript : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    public float framesPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+        index = index % sprites.Length;
+        spriteRenderer.sprite = sprites[index];
+
+    }
+}
